ca96f0d8b1b17c6c1d1f4496d250c997b38a48da
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-01-14 15:42:48 -0500

    attempt at othertest

---
+++ othertest/test_shared.py
@@ -0,0 +1,21 @@
+import sys
+
+_multiprocess_shared_ = True
+
+called = []
+
+def setup():
+  sys.stderr.write("setup called\n")
+  called.append("setup")
+
+def teardown():
+  sys.stderr.write("teardown called\n")
+  called.append("teardown")
+
+def test1():
+  sys.stderr.write("testing 1\n")
+  assert len([x for x in called if x == "setup"]) == 1
+
+def test2():
+  sys.stderr.write("testing 2\n")
+  assert len([x for x in called if x == "setup"]) == 1
