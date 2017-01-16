459c7b5e629a26fe280e3dd6909c5edd4eab6635
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-01-16 15:51:47 -0500

    cached fixture

--- multi_file/fixtures.py
+++ multi_file/fixtures.py
@@ -5,11 +5,16 @@ import time
 class Fixtures(unittest.TestCase):
   @classmethod
   def setup_class(cls):
+    if hasattr(cls, 'setup'):
+      return
     cls.setup = True
     sys.stderr.write("Setup Class\n")
     time.sleep(1)  # slow setup
 
   @classmethod
   def teardown_class(cls):
+    if hasattr(cls, 'eardown'):
+      return
+    cls.teardown = True
     sys.stderr.write("Teardown Class\n")
     time.sleep(1)  # slow setup
